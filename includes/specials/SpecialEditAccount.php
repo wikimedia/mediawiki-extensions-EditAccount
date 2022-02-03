1e26b170f9e5b72b3e167c82cdfd68a459f4865f
Umherirrender <umherirrender_de.wp@web.de>
2022-02-03 22:14:33 +0100

    Replace deprecated wfReadOnly()
    Bug: T283978
    Change-Id: I38ff68c20f27abbe65ca31151839d50a681fa8fd

--- includes/specials/SpecialEditAccount.php
+++ includes/specials/SpecialEditAccount.php
@@ -77,9 +77,7 @@ class EditAccount extends SpecialPage {
 		}
 
 		// Show a message if the database is in read-only mode
-		if ( wfReadOnly() ) {
-			throw new ReadOnlyError;
-		}
+		$this->checkReadOnly();
 
 		// If user is blocked, s/he doesn't need to access this page
 		if ( $user->getBlock() ) {
