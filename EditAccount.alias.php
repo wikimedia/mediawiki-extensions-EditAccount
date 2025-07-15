f2a303daabcd48024b58ea10939d074d9d0ee8f9
anterdc99 <xuminruixuan@live.cn>
2025-07-15 13:18:45 +0800

    EditAccount: Translate special page aliases into Chinese
    Change-Id: Ie8aeb0a56740f0ff8349d9b15ca315a4e63e6551

--- EditAccount.alias.php
+++ EditAccount.alias.php
@@ -18,3 +18,21 @@ $specialPageAliases['en'] = [
 $specialPageAliases['fi'] = [
 	'EditAccount' => [ 'Muokkaa käyttäjätunnusta', 'Sulje käyttäjätunnus', 'Sulje tunnus', 'Poista tunnus', 'Poista käyttäjätunnus' ],
 ];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'EditAccount' => [ 'EditAccount' ],
+	'CloseAccount' => [ 'CloseAccount' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'EditAccount' => [ '编辑账号', '禁用账号' ],
+	'CloseAccount' => [ '关闭账号' ],
+];
+
+/** Traditional Chinese (中文（繁體）) */
+$specialPageAliases['zh-hant'] = [
+	'EditAccount' => [ '編輯帳號', '停用帳號' ],
+	'CloseAccount' => [ '關閉帳號' ],
+];
